efe7a205ee423fb3952ae78f5e6d651245ad6d30
TimaGera <timgerasimchik@gmail.com>
2021-02-10 12:43:45 +0300

    rm file with sensitive data

--- src/main/java/com/epam/bookshop/controller/FrontController.java
+++ src/main/java/com/epam/bookshop/controller/FrontController.java
@@ -26,8 +26,7 @@ import java.io.IOException;
 public class FrontController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {processRequest(req, resp);
     }
 
     @Override
